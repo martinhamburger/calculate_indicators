5db8877c03608e8364da19c71a4ac44eda1c91a5
martinhamburger <320906540@qq.com>
2026-01-21 16:50:28 +0800

    计算过去时间点购买的收益率

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 """工具包初始化文件"""
 
 from .product_calculator import ProductNetValueCalculator
+from .buy_avg_calculator import BuyAvgReturnCalculator
 from .tools import process_single_file, generate_summary_file
 
-__all__ = ['ProductNetValueCalculator', 'process_single_file', 'generate_summary_file']
+__all__ = ['ProductNetValueCalculator', 'BuyAvgReturnCalculator', 'process_single_file', 'generate_summary_file']
